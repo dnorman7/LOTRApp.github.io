d079ba291363fc501c19f079ad49575bdd7a16da
Deanna Norman <deannaleenorman@msn.com>
2020-10-28 02:38:29 -0700

    had to find new api

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
 
+/*----- constants -----*/
+
+const BASE_URL = "https://the-office.p.rapidapi.com/1?rapidapi-key=3a776a8587mshb5a0763267e1f8ep177e80jsn31769ef05f15"
+/*----- app's state (variables) (data) -----*/
+let officeData; 
+
+/*----- cached element references -----*/
+const $container = $('#container');
+
+
+/*----- event listeners -----*/
+$container.on('click', '.card', handleClick);
+
+
+/*----- functions -----*/
+
+//using AJAX here to get data
+function getData() {
+    $.ajax(BASE_URL).then(function(data) {
+        console.log('Data: ', data);
+    }, function(error) {
+       console.log('Error: ', error);
+    });
+}
+
+function handleClick() {
+    alert('card was clicked');
+}
 
